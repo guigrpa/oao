ede59eba6ddd0bd91b222b5324838b21d0684835
G. Grau <ggrau@me.com>
2017-02-15 15:55:33 +0100

    Minor

--- src/utils/writeSpecs.js
+++ src/utils/writeSpecs.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const writeSpecs = (specPath, specs) => {
-  fs.writeFileSync(specPath, `${JSON.stringify(specs, null, '  ')}\n`, 'utf8');
+  fs.writeFileSync(specPath, `${JSON.stringify(specs, null, 2)}\n`, 'utf8');
 };
 
 export default writeSpecs;
